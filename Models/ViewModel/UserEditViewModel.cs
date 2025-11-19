1bdb4468bfcf78dd5f8f7437b5a2bb19b636308c
Qhien78 <hien3728195@gmail.com>
2025-11-19 13:53:38 +0700

    Sửa và xóa user

---
+++ Models/ViewModel/UserEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyKhoHang.Models.ViewModel
+{
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+
+        public string NewPassword { get; set; }
+    }
+}
