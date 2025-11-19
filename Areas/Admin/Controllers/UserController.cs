b8a581227091d4547eee7abe67d5961b3b1b3e2e
Qhien78 <hien3728195@gmail.com>
2025-11-19 12:44:46 +0700

    Thêm Create User

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyKhoHang.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Create() => View();
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Models.ViewModel.UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser {UserName = model.Email,Email = model.Email};
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    TempData["Success"] = "Created user successfully.";
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
