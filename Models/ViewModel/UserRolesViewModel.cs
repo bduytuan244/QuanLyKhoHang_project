179707e817672825629f56e6a4d00f04609e193d
Qhien78 <hien3728195@gmail.com>
2025-11-20 21:27:10 +0700

    chỉnh sửa quyền

---
+++ Models/ViewModel/UserRolesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace QuanLyKhoHang.Models.ViewModel
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public List<RoleSelection> Roles { get; set; }
+    }
+}
