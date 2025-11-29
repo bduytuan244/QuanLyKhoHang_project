775ea1990ad733657cdd178cce5e1042f73b445a
bduytuan244 <bdtuan244@gmail.com>
2025-11-30 00:23:25 +0700

    fix

---
+++ Controllers/SupplierApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer; // Quan trọng để dùng JwtBearerDefaults
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuanLyKhoHang.Models;
+using QuanLyKhoHang.Repository;
+
+namespace QuanLyKhoHang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // Dòng này để bảo mật, bắt buộc phải có Token mới lấy được danh sách
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class SupplierApiController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SupplierApiController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SupplierApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SupplierModel>>> GetSuppliers()
+        {
+            // Lấy tất cả nhà cung cấp từ database
+            return await _context.Supplier.ToListAsync();
+        }
+    }
+}
